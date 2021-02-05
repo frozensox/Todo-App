20f24818f2d4ab601a84030198770dd6a5f12426
frozensox <frozensox@icloud.com>
2021-02-05 21:55:30 +0900

    ルート定義（index, store, delete）

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,14 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\TaskController;
 
 Auth::routes();
-
+// Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'TaskController@index');
+    Route::post('/', 'TaskController@store');
+    Route::delete('/', 'TaskController@delete');
+    // Route::put('/', [TaskController::class, 'change']);
+    // Route::patch('/', [TaskController::class, 'sort']);
+    // Route::get('/edit/{item}', [TaskController::class, 'edit']);
+    // Route::post('/edit/{item}', [TaskController::class, 'update']);
+// });
 Route::get('/home', 'HomeController@index')->name('home');
